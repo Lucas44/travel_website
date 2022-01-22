e42ff89a814212025781530578c971bd2e32d34f
Lucas Nelson <lucasmnelson2020@gmail.com>
2022-01-22 16:39:32 -0500

    Uploading continents map and basic HTML layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-// import './App.css';
+import React, { useState } from "react";
+import Header from './components/Header';
+import Infobox from './components/Infobox';
+import ReactTooltip from "react-tooltip";
+import MapChart from "./components/MapChart";
+import "./index.css";
 
 function App() {
+  const [content, setContent] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header title='Travel Website'/>
+      <Infobox text='Travel information will go here'/>
+      <MapChart setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
     </div>
   );
 }
